bb0fdc058fa2d1fc12af95457614fd954878c3a7
pedro-git-projects <pedro.coding.contact@gmail.com>
2025-03-23 18:59:23 -0300

    [ref]full project refactor

--- src/zeebe/service.go
+++ src/zeebe/service.go
@@ -1,7 +1,6 @@
-package main
+package zeebe
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -14,15 +13,29 @@ import (
 	"github.com/camunda-community-hub/zeebe-client-go/v8/pkg/zbc"
 )
 
-func deployWorkflow(client zbc.Client, bpmnPath string) {
+type ZeebeService struct {
+	ZeebeClient    zbc.Client
+	OperateBaseURL string
+	HttpClient     *http.Client
+}
+
+func NewService(client zbc.Client, operateBaseURL string, httpClient *http.Client) *ZeebeService {
+	return &ZeebeService{
+		ZeebeClient:    client,
+		OperateBaseURL: operateBaseURL,
+		HttpClient:     httpClient,
+	}
+}
+
+// DeployWorkflow reads a BPMN file and deploys it via the Zeebe client.
+func (zs *ZeebeService) DeployWorkflow(bpmnPath string) {
 	ctx := context.Background()
 	file, err := os.ReadFile(bpmnPath)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to read BPMN file: %v", err))
 	}
 
-	deployCommand := client.NewDeployResourceCommand().AddResource(file, bpmnPath)
-
+	deployCommand := zs.ZeebeClient.NewDeployResourceCommand().AddResource(file, bpmnPath)
 	_, err = deployCommand.Send(ctx)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to deploy workflow: %v", err))
@@ -31,10 +44,11 @@ func deployWorkflow(client zbc.Client, bpmnPath string) {
 	fmt.Println("Workflow deployed:", bpmnPath)
 }
 
-func startProcess(client zbc.Client, processID string, variables map[string]interface{}) int64 {
+// StartProcess creates and starts a new process instance.
+func (zs *ZeebeService) StartProcess(processID string, variables map[string]interface{}) int64 {
 	ctx := context.Background()
 
-	cmd, err := client.NewCreateInstanceCommand().
+	cmd, err := zs.ZeebeClient.NewCreateInstanceCommand().
 		BPMNProcessId(processID).
 		LatestVersion().
 		VariablesFromMap(variables)
@@ -51,53 +65,13 @@ func startProcess(client zbc.Client, processID string, variables map[string]inte
 	return request.ProcessInstanceKey
 }
 
-func getOperateToken() string {
-	authURL := "http://localhost:18080/auth/realms/camunda-platform/protocol/openid-connect/token"
-	clientID := "zeebe"
-	clientSecret := "zecret"
-
-	data := []byte(fmt.Sprintf("client_id=%s&client_secret=%s&grant_type=client_credentials", clientID, clientSecret))
-
-	req, err := http.NewRequest("POST", authURL, bytes.NewBuffer(data))
-	if err != nil {
-		log.Fatalf("Failed to create auth request: %v", err)
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Failed to authenticate: %v", err)
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Failed to read auth response: %v", err)
-	}
-
-	var result map[string]interface{}
-	if err := json.Unmarshal(body, &result); err != nil {
-		log.Fatalf("Failed to parse auth response: %v", err)
-	}
-
-	accessToken, ok := result["access_token"].(string)
-	if !ok {
-		log.Fatalf("Authentication failed: %s", string(body))
-	}
-
-	fmt.Println("Obtained Operate API token successfully.")
-	return accessToken
-}
-
-func getProcessExecution(instanceKey int64, token string) {
-	url := fmt.Sprintf("http://localhost:8081/v1/flow-node-instances/%s", strconv.FormatInt(instanceKey, 10))
-
+// GetProcessExecution fetches and prints execution details for a process instance.
+func (zs *ZeebeService) GetProcessExecution(instanceKey int64, token string) {
+	url := fmt.Sprintf("%s/v1/flow-node-instances/%s", zs.OperateBaseURL, strconv.FormatInt(instanceKey, 10))
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("Authorization", "Bearer "+token)
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := zs.HttpClient.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -107,7 +81,8 @@ func getProcessExecution(instanceKey int64, token string) {
 	fmt.Println("Process Execution Details:", string(body))
 }
 
-func parseProcessState(jsonData []byte) {
+// ParseProcessState decodes JSON data to log the state of process elements.
+func (zs *ZeebeService) ParseProcessState(jsonData []byte) {
 	var response struct {
 		Instances []struct {
 			ID              string `json:"id"`
